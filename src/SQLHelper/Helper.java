c5efbd95def6f2ff734532711852701e927587e8
SigmaRaven <masterofthecheese@gmail.com>
2015-05-04 15:03:21 -0400

    Configuration which works

--- src/SQLHelper/Helper.java
+++ src/SQLHelper/Helper.java
@@ -5,30 +5,32 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Helper {
+	public static void main(String[] args) {
+		openDBConnection();
+	}
 	
 	public static Connection openDBConnection(){
-		System.out.println("nokay");
 		Connection conn = null;
 		
 		String driver = "com.mysql.jdbc.Driver";
-		String url = "jdbc:mysql://cs336-8.cs.rutgers.edu:3306/Luckerdogs";
-		String username = "csuser";
-		String password = "cs8d4896";
+		String url = "jdbc:mysql://localhost:3306/luckerdogs";
+		String username = "root";
+		String password = "lucky";
 
 		try {
 			Class.forName(driver);
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+			System.err.println("This went poorly.");
 			e.printStackTrace();
 			return null;
 		}
 		try {
 			conn = DriverManager.getConnection(url, username, password);
 		} catch (SQLException e) {
+			System.err.println("This went really poorly.");
 			e.printStackTrace();
 			return null;
 		}
-		System.out.println("ok");
 		return conn;
 	}
 	
@@ -42,7 +44,4 @@ public class Helper {
 		}
 	}
 	
-	public static void printPoop() {
-		System.out.print("poop");
-	}
 }
